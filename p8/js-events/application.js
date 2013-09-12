a52d5ce8ff6991a44b24bfaadcd791fdb2377886
nathanallen <thenathanator@gmail.com>
2013-09-11 22:10:18 -0700

    refactor to oojs

--- p8/js-events/application.js
+++ p8/js-events/application.js
@@ -1,45 +1,41 @@
 $(document).ready(function() {
   var todoTemplate = $.trim($('#todo_template').html());
+  var list = []
 
   function bindEvents() {
-    // $('.add').on('click', $('.todo_list'), adder)
-
-    $('.add').on('click', $('.todo_list'), function(){
-      console.log("add clicked")
-      console.log()
-      // input = $(this).val()
-      // $('.todo_list').append(buildTodo(input))
-
-    })
-
-
-
-    //$('a.delete').on('click', deleter)
-    //$('.todo a.delete').on('click', function(){$(this).parents('.todo').remove()})
-    // $('a.delete').on('click', deleter)
-    // $('a.complete').on('click', completer)
-
-    // Bind functions which add, remove, and complete todos to the appropriate
-    // elements
+    $('.add').on('click', makeTodoItem)
   }
 
-  //Create functions to add, remove and complete todos
-  var adder = function(){
-    console.log("add clicked")
-    console.log($(this))
-    input = $(this).val()
-    $('.todo_list').append(buildTodo(input))
-  }
-  var deleter = function(){
-    console.log("delete clicked")
-    //$('.todo a.delete').parents('.todo').remove()
-  }
-  var completer = function(){
-    console.log("complete clicked")
+  function makeTodoItem(){
+    var t = new Todo()
+    list.push(t)
+    t.deleteListener()
+    //t.completeListener()
   }
 
-  $('.todo a.delete').parents('.todo').remove()
-  
+  function Todo(){
+    this.text = $('input.todo').val()
+    this.item = $('.todo_list').append(buildTodo(this.text))
+    //this.complete = false
+  }
+
+  // Todo.prototype.completeListener = function(){
+  //   console.log("setting complete listener")
+  // //  this.complete = true
+  //   //$('a.delete').on('click', deleter)
+  // }
+
+  Todo.prototype.deleteListener = function(){
+    console.log("setting delete listener")
+    this.item[0].addEventListener("click", function(){this.remove()})
+
+  }
+
+  Todo.prototype.listener = function (){
+    console.log(this.item[0])
+    //this.item[0].addEventListener("click", function(){console.log("clicked")})
+  }
+
 
   function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
@@ -49,7 +45,8 @@ $(document).ready(function() {
     // Returns the jQueryDOMElement to be used elsewhere.
     return $todo;
   }
-  
+
 
   bindEvents();
 });
+
